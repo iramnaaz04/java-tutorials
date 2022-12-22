707964908672ebbc9862e1659eaf3646ea2a834d
Iramnaaz <shaikhiramnaaz04@gmail.com>
2022-12-22 21:35:27 +0530

    Comparing numbers and Grades

---
+++ Strings/Strings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Strings {
+	 public static void main(String[] args) {
+//		 int a = 7;
+	//	 float b = 6.97907f;
+	//	 System.out.printf("the value of a is %d and of b is %f", a,b);
+	//	 System.out.format("the value of a is %d and of b is %f", a,b);
+	Scanner sc = new Scanner(System.in);
+	String st = sc.nextLine();
+	System.out.println(st);
+		 
+	 } 
+}
