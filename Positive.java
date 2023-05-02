8b5030e0912502267c1c349e32a25ca3f6d5e996
Iramnaaz <shaikhiramnaaz04@gmail.com>
2023-05-02 14:09:33 +0530

    Reversing a number entered by user

---
+++ Positive.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Positive{
+	public static void main(String[] args){
+		int no,z=0,p=0,n=0;
+		Scanner sc = new Scanner(System.in);
+		char ans = 'y';
+		System.out.println("Enter a no: ");
+		no = sc.nextInt();
+		while(ans = y)
+		{
+			System.out.println("Enter a no: ");
+			if(no > 0 )
+			{
+				p = p +1;
+			}
+			else if (no < 0)
+			{
+				n = n + 1;
+			}
+			else if (n0 == 0)
+			{
+				z = z + 1;
+			}
+		}
+		System.out.println("Do you want to count");
+		ans = getchar();
+		
+	}
+	System.out.printf("%d,%d,%d",p,n,z);
+	getch();
+}
