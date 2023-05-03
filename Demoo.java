3c5bb1f3892dc56acf5627e7596a6d0334006878
Iramnaaz <shaikhiramnaaz04@gmail.com>
2023-05-03 13:59:47 +0530

    To print a ab abc

---
+++ Demoo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Demoo{
+	public static void main(String[] args){
+		int roll_no,marks,n;
+		boolean female;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.println("Enter roll_no of student: ");
+		System.out.println("Enter marks: ");
+		System.out.println("Gender: ");
+		
+	}
+}
---
+++ Print_pattern/Print.java
@@ -0,0 +1,24 @@
+/* TO PRINT :
+a
+a b
+a b c
+*/
+public class Print{
+	public static void main(String[] args){
+		int i,j;
+		char val = 64;
+		for(i=1; i<=3; i++)
+		{
+			for(j=1;j<=i; j++)
+		
+	
+			{
+		System.out.printf("%c",val + j);		
+		
+		}
+		System.out.println("\n");
+		}
+		System.out.print(" ");
+		
+	}
+}
